5bdfb4917c817b9c956caaafa25660f2d126ff0e
DeniLiem <bebu1047@gmail.com>
2021-10-02 15:49:18 +0700

    test version

--- sever.js
+++ sever.js
@@ -15,13 +15,14 @@ app.use(express.static('public'))
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
-db.on('error', err => { console.error(err) })
-db.once('open', () => { console.log('Database is connected') })
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Database is connected'))
 
 app.use('/', indexRouter)
+app.use('/db', (req, res) => {
+    res.send(process.env.DATABASE_URL)
+})
 
 app.listen(process.env.PORT || 5000, () => {
     console.log('Sever is connected at port 5000')
 })
-
-console.log('hey')
