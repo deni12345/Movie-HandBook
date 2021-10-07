f3a8e7f86e7461a68abb60903da9185beb5b67ea
DeniLiem <bebu1047@gmail.com>
2021-10-07 13:34:24 +0700

    index: create index route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const router = express.Router()
+const Movie = require('../models/movie')
 
-router.get('/', (req, res) => {
-    res.render('./user/login')
+router.get('/', async(req, res) => {
+    let movies
+    try {
+        movies = await Movie.find().sort({ createDate: 'desc' }).limit(10).exec()
+
+    } catch (error) {
+        movies = []
+    }
+
+    res.render('index', {
+        movies: movies
+    })
 })
-
 module.exports = router
